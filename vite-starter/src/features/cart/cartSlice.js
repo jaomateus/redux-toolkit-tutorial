2a994baeb5280f4c5ee23d263a678874a13e71ab
Joao Mateus <jaomateus@gmail.com>
2023-04-08 13:33:58 +0100

    finish exercise

--- vite-starter/src/features/cart/cartSlice.js
+++ vite-starter/src/features/cart/cartSlice.js
@@ -10,11 +10,28 @@ const initialState = {
 	isLoading: true,
 };
 
-export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
-	return fetch(url)
-		.then((resp) => resp.json())
-		.catch((err) => console.log(error));
-});
+// export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
+// 	return fetch(url)
+// 		.then((resp) => resp.json())
+// 		.catch((err) => console.log(error));
+// });
+
+export const getCartItems = createAsyncThunk(
+	"cart/getCartItems",
+	async (name, thunkAPI) => {
+		try {
+			// console.log(name);
+			// console.log(thunkAPI);
+			// console.log(thunkAPI.getState());
+			// thunkAPI.dispatch(openModal());
+			const resp = await axios(url);
+
+			return resp.data;
+		} catch (error) {
+			return thunkAPI.rejectWithValue("something went wrong");
+		}
+	},
+);
 
 const cartSlice = createSlice({
 	name: "cart",
